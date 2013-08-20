c28b039ae37c10031caed053cab4dc08d3e16e5e
Rhys <rhys@dataminr.com>
2013-08-20 10:43:07 -0400

    put in comments

--- aplus.js
+++ aplus.js
@@ -7,21 +7,31 @@ var State = {
 var Aplus = {
 	state: State.PENDING,
 	changeState: function(state, value) {
+
+		// catch changing to same state (perhaps trying to change the value)
 		if ( this.state == state ) {
 			throw new Error("can't transition to same state: " + state);
 		}
+
+		// trying to change out of fulfilled or rejected
 		if ( this.state == State.FULFILLED ||
 				this.state == State.REJECTED ) {
 			throw new Error("can't transition from current state: " + state);
 		}
+
+		// if second argument isn't given at all (passing undefined allowed)
 		if ( state == State.FULFILLED &&
 				arguments.length < 2 ) {
 			throw new Error("transition to fulfilled must have a non null value");
 		}
+
+		// if a null reason is passed in
 		if ( state == State.REJECTED &&
-				arguments.length < 2 ) {
+				value == null ) {
 			throw new Error("transition to rejected must have a non null reason");
 		}
+
+		//change state
 		this.state = state;
 		return this.state;
 	}
