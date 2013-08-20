11ec799fde996e9e193495d995c2f2d6e288080d
Rhys <rhys@dataminr.com>
2013-08-20 12:40:22 -0400

    make compliant with promises tests

--- aplus.js
+++ aplus.js
@@ -12,25 +12,25 @@ var Aplus = function() {
 
 			// catch changing to same state (perhaps trying to change the value)
 			if ( this.state == state ) {
-				throw new Error("can't transition to same state: " + state);
+				return new Error("can't transition to same state: " + state);
 			}
 
 			// trying to change out of fulfilled or rejected
 			if ( this.state == State.FULFILLED ||
 					this.state == State.REJECTED ) {
-				throw new Error("can't transition from current state: " + state);
+				return new Error("can't transition from current state: " + state);
 			}
 
 			// if second argument isn't given at all (passing undefined allowed)
 			if ( state == State.FULFILLED &&
 					arguments.length < 2 ) {
-				throw new Error("transition to fulfilled must have a non null value");
+				return new Error("transition to fulfilled must have a non null value");
 			}
 
 			// if a null reason is passed in
 			if ( state == State.REJECTED &&
-					value == null ) {
-				throw new Error("transition to rejected must have a non null reason");
+					arguments.length < 2 ) {
+				return new Error("transition to rejected must have a non null reason");
 			}
 
 			//change state
